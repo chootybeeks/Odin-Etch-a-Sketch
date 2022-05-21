b8aff08a10c5d2da24c697b468f266f56c0c9e0c
chootybeeks <quadi.d.banks@gmail.com>
2022-05-21 13:59:58 -0400

    Add eventlistener to each div for changing the background colol

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const createGrid = (rows, columns) => {
     //for loop to create a div for each 'cell' and append it to the sketchContainer
     for (let i = 0; i < rows * columns; i++) {
         let cell = document.createElement('div')
-        cell.textContent = i + 1
+        cell.textContent = ''
         sketchContainer.appendChild(cell).className = 'grid-item'
+        cell.addEventListener('mousemove', () => {
+            cell.style.backgroundColor = 'black' 
+        })
     }
 }
 
 createGrid(16, 16)
 
-/*let cell = document.createElement('div')
-            cell.textContent = j + 1
-            sketchContainer.appendChild(cell).className = 'grid-item' */
 
 
 
