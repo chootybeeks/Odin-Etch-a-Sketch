5d0120f59a8f74c970be7c1309e79111dbc8cf07
chootybeeks <quadi.d.banks@gmail.com>
2022-05-21 16:27:53 -0400

    Refactor createGrid function

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const sketchContainer = document.getElementById('sketch-grid-container')
 
 //Create grid using CSS grid
 const createGrid = (rows, columns) => {
-    sketchContainer.style.setProperty('--grid-rows', rows)
-    sketchContainer.style.setProperty('--grid-columns', columns)
+    //sketchContainer.style.setProperty('--grid-rows', rows)
+    //sketchContainer.style.setProperty('--grid-columns', columns)
 
     //for loop to create a div for each 'cell' and append it to the sketchContainer
     for (let i = 0; i < rows * columns; i++) {
         let cell = document.createElement('div')
-        cell.textContent = ''
+        sketchContainer.style.gridTemplateRows = `repeat(${columns}, 1fr)`
+        sketchContainer.style.gridTemplateColumns = `repeat(${rows}, 1fr)`
         sketchContainer.appendChild(cell).className = 'grid-item'
         cell.addEventListener('mousemove', () => {
             cell.style.backgroundColor = 'black' 
